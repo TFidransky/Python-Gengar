a5d3327e7eca51eea6a08f3c7dc24594f979690a
Tomas Fidransky <tom.fidransky@gmail.com>
2022-12-03 19:03:57 +0100

    vetsi_update_oprava

--- Projekty/2_ukol/prutok_reky.py
+++ Projekty/2_ukol/prutok_reky.py
@@ -1,26 +1,54 @@
 import csv
-from datetime import datetime
+from math import floor
 
-def openCSV():
-    with open ("vstup.csv", encoding = "utf-8", newline="") as f:
-        rows = csv.reader(f, delimiter = ",")
-        for row in rows:
-            rok = row [-4]
-            mesic = row [-3]
-            den = row [-2]
-            prutok = row[-1]    
 
-        
-#def kontrola():
-#    try:
-        
-#def vypocet7(reader):
-#    with open("vystup_7dni.csv","w", encoding = "utf-8", newline="") as f7:
+def openCSV() -> []:
+    file = open("vstup.csv", "r")
+    reader = csv.reader(file)
+    rows = []
+    for row in reader:
+        rows.append(row)
 
-  
+    return rows
 
-#def vypocet365(reader):
-#    with open("vystup_rok.csv", "w", encoding = "utf-8", newline="") as f365:
+def parseToWeeks(rows: []):
+    i = 0
+    weeks = []
 
-openCSV()
+    #nacti cele tydny
+    for number_of_week in range(0, floor(len(rows) / 7)):
+        weeks.append([])
+        for day in range(0, 7):
+            weeks[number_of_week].append(rows[i])
+            i = i + 1
+
+    #nacti posledni tyden
+    if i != len(weeks):
+        weeks.append([])
+        for day in range(0, len(rows) - i):
+            weeks[len(weeks) - 1].append(rows[i])
+
+    return weeks
+
+def calculateWeekAverage(week):
+    amounts = []
+    for day in week:
+        amounts.append(float(day[3].strip()))
+
+    return sum(amounts) / len(amounts)
+
+def writeWeekToCsv(first_day, average):
+    file = open('output.csv', 'a')
+    writer = csv.writer(file)
+    first_day[3] = '{0:.4f}'.format((floor(average*10000)/10000)).__str__()
+    writer.writerow(first_day)
+
+def main():
+    rows_of_csv = openCSV()
+    weeks = parseToWeeks(rows_of_csv)
+    for week in weeks:
+        writeWeekToCsv(week[0], calculateWeekAverage(week))
+
+main()
+
 
