3a381e1c1c505ea795cbbd89bec17bc18cd97c61
Tomas Fidransky <tom.fidransky@gmail.com>
2022-12-03 19:01:38 +0100

    vetsi_update

--- Projekty/2_ukol/prutok_reky.py
+++ Projekty/2_ukol/prutok_reky.py
@@ -3,19 +3,23 @@ from datetime import datetime
 
 def openCSV():
     with open ("vstup.csv", encoding = "utf-8", newline="") as f:
-        reader = csv.reader(f, delimiter = ",")
+        rows = csv.reader(f, delimiter = ",")
+        for row in rows:
+            rok = row [-4]
+            mesic = row [-3]
+            den = row [-2]
+            prutok = row[-1]    
 
+        
 #def kontrola():
 #    try:
         
-
-#def vypocet():
-#    celk = 0
-
-#def pocitej7():
+#def vypocet7(reader):
 #    with open("vystup_7dni.csv","w", encoding = "utf-8", newline="") as f7:
 
-#def pocitej365():
+  
+
+#def vypocet365(reader):
 #    with open("vystup_rok.csv", "w", encoding = "utf-8", newline="") as f365:
 
 openCSV()
