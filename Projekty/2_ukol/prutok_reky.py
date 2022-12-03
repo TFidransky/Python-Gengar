d86bdab687ae3c180d1f98797603a6c90d9fb402
Tomas Fidransky <tom.fidransky@gmail.com>
2022-12-03 13:36:06 +0100

    Create prutok_reky.py

---
+++ Projekty/2_ukol/prutok_reky.py
@@ -0,0 +1,21 @@
+import csv
+from datetime import datetime
+
+def openCSV():
+    with open ("vstup.csv", encoding = "utf-8", newline="") as f:
+        reader = csv.reader(f, delimiter = ",")
+
+#def kontrola():
+#    try:
+
+#def vypocet():
+#    celk = 0
+
+#def pocitej7():
+#    with open("vystup_7dni.csv","w", encoding = "utf-8", newline="") as f7:
+
+#def pocitej365():
+#    with open("vystup_rok.csv", "w", encoding = "utf-8", newline="") as f365:
+
+openCSV()
+
