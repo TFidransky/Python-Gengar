764d64b82f923410e766aaeec637577796c3acf8
Tomas Fidransky <116719883+TFidransky@users.noreply.github.com>
2022-12-14 12:38:42 +0100

    Omylem do špatného repozitáře

--- Projekty/3_ukol/Praha_kontejnery.py
+++
@@ -1,8 +0,0 @@
-import json
-from geopy.distance import geodesic
-
-# načteme vstupní GeoJSON soubory
-with open('adresy.geojson') as f:
-    adresy = json.load(f)
-with open('kontejnery.geojson') as f:
-    kontejnery = json.load(f)
